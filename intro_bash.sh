80f159d170b76c70e3623a9f0074e22cd01c367d
juliehub <67776121+juliehub@users.noreply.github.com>
2020-07-24 16:29:20 +1000

    Update intro_bash.sh

--- intro_bash.sh
+++ intro_bash.sh
@@ -1,3 +1,11 @@
 #**1. Write a Bash script which accepts  as input and displays a greeting: "Welcome (name)"**
 read -p "Enter user:" user
 echo "Welcome $user"
+
+#2. Use a for loop to display the natural numbers from 1 to 50
+x=1
+while [ $x -le 50 ]
+do
+    echo $x
+    x=$(($x + 1))
+done
