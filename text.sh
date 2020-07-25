08a0f7259ddad43814922879f84c172cc660ba80
juliehub <67776121+juliehub@users.noreply.github.com>
2020-07-25 15:15:38 +1000

    Update text.sh

--- text.sh
+++ text.sh
@@ -5,3 +5,9 @@ do
     echo $line | cut -c3
 done
 ################################
+# 2. Display the 2nd and 7th character from each line of text.
+while read line
+do
+	echo $line | cut -c2,7
+done
+################################
