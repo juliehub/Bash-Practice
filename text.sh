5225780c6606cbed0417c63211e27d919151dc2d
juliehub <67776121+juliehub@users.noreply.github.com>
2020-07-25 17:01:12 +1000

    Update text.sh

--- text.sh
+++ text.sh
@@ -11,3 +11,19 @@ do
 	echo $line | cut -c2,7
 done
 ################################
+# 3. Display a range of characters starting at the 2nd position of a string and ending at the 7th position
+while read line
+do
+	echo $line | cut -c2-7
+done
+################################
+# 4. Display the first four characters from each line of text.
+while read line
+do
+	echo $line | cut -c-4
+done
+################################
+# 5. Given a tab delimited file with several columns (tsv format) print the first three fields.
+cut -f 1-3
+################################
+#  
