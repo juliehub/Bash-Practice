dae06d2af0f4eb9e8e1fd4f83daca4447e35e122
juliehub <67776121+juliehub@users.noreply.github.com>
2020-07-26 21:34:09 +1000

    Update array.sh

--- array.sh
+++ array.sh
@@ -2,4 +2,8 @@
 arr=($(cat))  
 echo ${arr[*]}
 #####################
-# 2.
+# 2. Slice the array and display only the elements lying between positions  and , both inclusive. Indexing starts from from .
+arr=($(cat))  
+echo ${arr[@]:3:5}
+#####################
+# 3.
